9c92e3170c4a907dceee3cfc73f84eb58f88379d
Punitharaj S <83452431+Punitharaj-s@users.noreply.github.com>
2023-04-11 17:28:35 +0530

    script check

--- chartComponent.js
+++ chartComponent.js
@@ -3,9 +3,9 @@
     tmpl.innerHTML = 
     `</script><div id="donutchart" style="width: 900px; height: 500px;"></div>` ;   
     // adding google script//
-    let scriptEle = document.createElement("script");
-    scriptEle.setAttribute("src", "https://www.gstatic.com/charts/loader.js");
-    document.body.appendChild(scriptEle);
+//     let scriptEle = document.createElement("script");
+//     scriptEle.setAttribute("src", "https://www.gstatic.com/charts/loader.js");
+//     document.body.appendChild(scriptEle);
     
     class PerformanceHelp extends HTMLElement {
         constructor() {
@@ -17,26 +17,28 @@
               
             let shadowRoot = this.attachShadow({mode: "open"});
             shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            google.charts.load("current", {packages:["corechart"]});
-            google.charts.setOnLoadCallback(drawChart);
-            function drawChart() {
-                var jsonArray = fetch("https://teamtreehouse.com/profiles/matthew.json").then((response)=>response.json());
-                var array = JSON.parse(jsonArray);
-                console.log(array['id']);
-                scriptEle.addEventListener("error", (ev) => {
-            console.log("Error on loading file", ev);
-            });
-                //var data = google.visualization.arrayToDataTable(array);
+            //error check
+            console.log("script working");
+//             google.charts.load("current", {packages:["corechart"]});
+//             google.charts.setOnLoadCallback(drawChart);
+//             function drawChart() {
+//                 var jsonArray = fetch("https://teamtreehouse.com/profiles/matthew.json").then((response)=>response.json());
+//                 var array = JSON.parse(jsonArray);
+//                 console.log(array['id']);
+//                 scriptEle.addEventListener("error", (ev) => {
+//             console.log("Error on loading file", ev);
+//             });
+//                 //var data = google.visualization.arrayToDataTable(array);
 
-//         var options = {
-//           title: 'Values',
-//           pieHole: 0.4,
-//         };
+// //         var options = {
+// //           title: 'Values',
+// //           pieHole: 0.4,
+// //         };
 
-//         var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-//         chart.draw(data, options);
-      }
-        }       
+// //         var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+// //         chart.draw(data, options);
+//       }
+//         }       
         
     }
 
